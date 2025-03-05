4b43b4ec68ec3bca767496b9d16e974ff986d6c3
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-03-06 02:29:36 +0600

    refact: reviews

--- app/reviews/admin.py
+++ app/reviews/admin.py
@@ -19,7 +19,9 @@ class QuestionAnswerInline(TabularInline):
 @admin.register(StudentReview)
 class StudentReviewAdmin(ModelAdmin):
     list_display = ('id', 'full_name', 'course', 'category', 'created_at', 'is_published')
+    list_display_links = ('id', 'full_name')
     list_filter = ('is_published', 'category', 'created_at')
+    list_editable = ('is_published',)
     search_fields = ('full_name',)
     ordering = ('-created_at',)
     inlines = [QuestionAnswerInline]
