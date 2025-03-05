dcfc654e96233db5d0dce43b7bfbb90407a9979e
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-03-06 02:27:40 +0600

    refact: reviews

--- app/reviews/serializers.py
+++ app/reviews/serializers.py
@@ -14,7 +14,7 @@ class QuestionAnswerSerializer(serializers.ModelSerializer):
         fields = ['question', 'answer']
 
 
-class StudentReviewListSerializer(serializers.ModelSerializer):
+class StudentReviewBaseSerializer(serializers.ModelSerializer):
     question_answers = QuestionAnswerSerializer(many=True, read_only=True)
     category = CategorySerializer(read_only=True)
 
@@ -22,19 +22,16 @@ class StudentReviewListSerializer(serializers.ModelSerializer):
         model = StudentReview
         fields = ['full_name', 'slug', 'photo', 'course', 'category',
                   'question_answers', 'created_at']
+
+
+class StudentReviewDetailSerializer(StudentReviewBaseSerializer):
+    pass
+
+
+class StudentReviewListSerializer(StudentReviewBaseSerializer):
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         representation['question_answers'] = representation['question_answers'][:3]
-        
+
         return representation
-
-
-class StudentReviewDetailSerializer(serializers.ModelSerializer):
-    question_answers = QuestionAnswerSerializer(many=True, read_only=True)
-    category = CategorySerializer(read_only=True)
-
-    class Meta:
-        model = StudentReview
-        fields = ['full_name', 'slug', 'photo', 'course', 'category',
-                  'question_answers', 'created_at']
