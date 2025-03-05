79bab76df48da86f93543d87b889f89093615d41
KUMA-ORIGINAL <asanovkurmanbek342@gmail.com>
2025-03-06 02:44:42 +0600

    refact: specialties

--- app/specialties/models/specialty.py
+++ app/specialties/models/specialty.py
@@ -2,23 +2,27 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 
 
+
 class Specialty(models.Model):
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(unique=True)
-    specialty = models.CharField(max_length=255)
-    description = models.TextField()
+    title = models.CharField(max_length=255, verbose_name=_("Название"))
+    slug = models.SlugField(unique=True, verbose_name=_("Слаг"))
+    specialty = models.CharField(max_length=255, verbose_name=_("Специальность"))
+    description = models.TextField(verbose_name=_("Описание"))
     preview_photo = models.FileField(upload_to='specialty/preview_photos/%Y/%m',
                                      blank=True, verbose_name=_("Превью Фото"))
     photo = models.FileField(upload_to='specialty/photos/%Y/%m',
                              blank=True, verbose_name=_("Фото"))
-
     category = models.ForeignKey('SpecialtyCategory', on_delete=models.CASCADE,
-                                 related_name='specialty')
+                                 related_name='specialty', verbose_name=_("Категория"))
     training_program = models.ForeignKey('TrainingProgram', on_delete=models.CASCADE,
-                                         related_name='specialty', blank=True, null=True)
-    student_projects = models.ManyToManyField('StudentProject', related_name='specialty')
+                                         related_name='specialty', blank=True, null=True, verbose_name=_("Программа обучения"))
+    student_projects = models.ManyToManyField('StudentProject', related_name='specialty', verbose_name=_("Проекты студентов"))
     cv = models.ForeignKey('CV', on_delete=models.CASCADE, related_name='specialty',
-                           blank=True, null=True)
+                           blank=True, null=True, verbose_name=_("Резюме"))
+
+    class Meta:
+        verbose_name = _("Специальность")
+        verbose_name_plural = _("Специальности")
 
     def __str__(self):
         return self.title
--- app/specialties/models/specialty_category.py
+++ app/specialties/models/specialty_category.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class SpecialtyCategory(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, verbose_name='Название категории')
     photo = models.FileField(upload_to='specialty/photos/%Y/%m',
                              blank=True, verbose_name=_("Фото"))
 
